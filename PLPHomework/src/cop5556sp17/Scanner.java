e7cc693c758627e1e91a3f1782fb11d388f4b160
tony scialo <arscialo@gmail.com>
2017-02-26 16:23:50 -0500

    added items to token class that were specified in the assignment-3 section

--- PLPHomework/src/cop5556sp17/Scanner.java
+++ PLPHomework/src/cop5556sp17/Scanner.java
@@ -908,6 +908,50 @@ public class Scanner {
 				return false;
 		}
 		
+		@Override
+		  public int hashCode() {
+		   final int prime = 31;
+		   int result = 1;
+		   result = prime * result + getOuterType().hashCode();
+		   result = prime * result + ((kind == null) ? 0 : kind.hashCode());
+		   result = prime * result + length;
+		   result = prime * result + pos;
+		   return result;
+		  }
+
+		  @Override
+		  public boolean equals(Object obj) {
+		   if (this == obj) {
+		    return true;
+		   }
+		   if (obj == null) {
+		    return false;
+		   }
+		   if (!(obj instanceof Token)) {
+		    return false;
+		   }
+		   Token other = (Token) obj;
+		   if (!getOuterType().equals(other.getOuterType())) {
+		    return false;
+		   }
+		   if (kind != other.kind) {
+		    return false;
+		   }
+		   if (length != other.length) {
+		    return false;
+		   }
+		   if (pos != other.pos) {
+		    return false;
+		   }
+		  return true;
+		  }
+
+		 
+
+		  private Scanner getOuterType() {
+			  return Scanner.this;
+		  }
+		
 	}
 	
 	/**
